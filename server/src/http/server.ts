d27010d807e98af458f84d5e8230f812c99d104c
gp208 <gustavocprieto@gmail.com>
2025-05-13 11:36:15 -0300

    config cors

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -4,8 +4,14 @@ import { createCompletionRoute } from './routes/create-completion'
 import { createGoalRoute } from './routes/create-goal'
 import { getPendingGoalsRoute } from './routes/get-pending-goals'
 import { getWeekSummaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+    origin: '*'
+})
+
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
