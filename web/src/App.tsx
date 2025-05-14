bbe94c44769c270fbde38d03ed47e24eacb6b778
gp208 <gustavocprieto@gmail.com>
2025-05-13 22:21:35 -0300

    simplify button code

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import {Plus} from 'lucide-react'
 import logo from './assets/logo-in-orbit.svg'
+import { Button } from './components/ui/button'
 
 export function App() {
 
@@ -9,10 +10,8 @@ export function App() {
       <p className='text-zinc-300 leading-relaxed max-w-80 text-center'>
         Você ainda não cadastrou nenhuma meta. Que tal cadastrar uma agora mesmo?
       </p>
-      <button type='button' className='px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 
-      flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600'>
-        <Plus className='size-4'/>Cadastrar meta
-      </button>
+
+      <Button><Plus className='size-4'/>Cadastrar meta</Button>
     </div>
   )
 }
