f5bab4f8ee712cee8e1eacabceece13459829de6
gp208 <gustavocprieto@gmail.com>
2025-05-14 15:36:11 -0300

    goals summary screen

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { NoGoals } from './components/no-goals'
+import { Summary } from './components/summary'
 
 export function App() {
 
   return (
     <Dialog>
       <NoGoals/>
+      <Summary/>
       <CreateGoal/>
     </Dialog>
   )
