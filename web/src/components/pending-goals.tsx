60981039ecfba90045849ec1b1ee02dc48774866
gp208 <gustavocprieto@gmail.com>
2025-05-16 19:03:51 -0300

    invalidate queries to make reloading unnecessary

--- web/src/components/pending-goals.tsx
+++ web/src/components/pending-goals.tsx
@@ -1,32 +1,36 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { getPendingGoals } from '../http/get-pending-goals';
 import { Plus } from 'lucide-react';
 import { OutlineButton } from './ui/outline-button';
 import { createGoalCompletion } from '../http/create-goal-completion';
 
 export function PendingGoals() {
-    const {data} = useQuery({
-      queryKey: ['pending-goals'],
-      queryFn: getPendingGoals
-    })
-    if(!data) return null
+  const queryClient = useQueryClient()
+  
+  const {data} = useQuery({
+    queryKey: ['pending-goals'],
+    queryFn: getPendingGoals
+  })
+  if(!data) return null
 
-    async function handleCompletedGoal(goalId: string) {
-      await createGoalCompletion(goalId)
-    }
+  async function handleCompletedGoal(goalId: string) {
+    await createGoalCompletion(goalId)
+    queryClient.invalidateQueries({queryKey: ['pending-goals']})
+    queryClient.invalidateQueries({queryKey: ['summary']})
+  }
 
-    return (
-        <div className='flex flex-wrap gap-3'>
-            {data.map(goal => {
-                return(
-                  <OutlineButton
-                    key={goal.id}
-                    disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
-                    onClick={() => handleCompletedGoal(goal.id)}
-                  >
-                    <Plus className='size-4 text-zinc-600'/>{goal.title}
-                  </OutlineButton>)
-            })}
-          </div>
-    )
+  return (
+    <div className='flex flex-wrap gap-3'>
+      {data.map(goal => {
+        return(
+          <OutlineButton
+            key={goal.id}
+            disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
+            onClick={() => handleCompletedGoal(goal.id)}
+          >
+            <Plus className='size-4 text-zinc-600'/>{goal.title}
+          </OutlineButton>)
+      })}
+    </div>
+  )
 }
