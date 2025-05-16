39d1d3f1732bdee34d4a7c5c50b8d4098d3dede8
gp208 <gustavocprieto@gmail.com>
2025-05-15 23:01:06 -0300

    listing of pending goals

---
+++ web/src/components/pending-goals.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import { getPendingGoals } from '../http/get-pending-goals';
+import { Plus } from 'lucide-react';
+import { OutlineButton } from './ui/outline-button';
+
+export function PendingGoals() {
+    const {data} = useQuery({
+      queryKey: ['pending-goals'],
+      queryFn: getPendingGoals
+    })
+    if(!data) return null
+
+    return (
+        <div className='flex flex-wrap gap-3'>
+            {data.map(goal => {
+                return(
+                  <OutlineButton key={goal.id} disabled={goal.completionCount >= goal.desiredWeeklyFrequency}>
+                    <Plus className='size-4 text-zinc-600'/>{goal.title}
+                  </OutlineButton>)
+            })}
+          </div>
+    )
+}
